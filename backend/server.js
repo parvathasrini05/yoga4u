c73fe260df1a5433cfa2bcf99af0469f405be3a5
parvathasrini05 <parvathasrini5@gmail.com>
2025-12-29 12:16:25 +0530

    connection

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB(); // ✅ now this will work
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Yoga4U Backend Running 🚀");
-});
-
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
